197dc6032fabb62c7e02a648d98a0c5a673f9c5b
Jan Mikeš <j.mikes@me.com>
2023-11-02 21:21:48 +0100

    Authentication with auth0 (#10)
    * Authentication with auth0
    
    * Remove tests
    
    * fixup! Remove tests

--- config/packages/security.php
+++ config/packages/security.php
@@ -2,8 +2,20 @@
 
 declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Auth0\Symfony\Security\UserProvider;
+use Symfony\Config\SecurityConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (SecurityConfig $securityConfig): void {
+    $securityConfig
+        ->provider('auth0_provider')
+        ->id(UserProvider::class);
 
+    $securityConfig->firewall('dev')
+        ->pattern('^/(_(profiler|wdt)|css|images|js)/')
+        ->security(false);
+
+    $securityConfig->firewall('main')
+        ->pattern('/(muj-profil|pridat-cas)')
+        ->provider('auth0_provider')
+        ->customAuthenticators(['auth0.authenticator']);
 };
--- config/routes.php
+++ config/routes.php
@@ -7,6 +7,12 @@ use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 return static function (RoutingConfigurator $routingConfigurator): void {
     $routingConfigurator->import(__DIR__ . '/../src/Controller', 'annotation');
 
+    $routingConfigurator->add('login', '/login')
+        ->controller('Auth0\Symfony\Controllers\AuthenticationController::login');
+
+    $routingConfigurator->add('callback', '/auth/callback')
+        ->controller('Auth0\Symfony\Controllers\AuthenticationController::callback');
+
     $routingConfigurator->add('logout', '/logout')
-        ->methods(['GET']);
+        ->controller('Auth0\Symfony\Controllers\AuthenticationController::logout');
 };

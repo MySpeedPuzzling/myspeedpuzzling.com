7de1cfc35090e66a303e9350751d3372be014fb7
Jan Mikeš <j.mikes@me.com>
2023-11-03 12:56:38 +0100

    Static privacy policy and terms of service pages

---
+++ src/Controller/PrivacyPolicyController.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace SpeedPuzzling\Web\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class PrivacyPolicyController extends AbstractController
+{
+    #[Route(path: '/privacy-policy', name: 'privacy_policy', methods: ['GET'])]
+    public function __invoke(Request $request): Response
+    {
+        return $this->render('privacy-policy.html.twig');
+    }
+}

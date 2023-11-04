1c8aad2783ae2c7ac516c3f14bbe572c5b683872
Jan Mikeš <j.mikes@me.com>
2023-11-04 20:38:36 +0100

    Puzzle service offering

---
+++ src/Controller/PuzzleServiceController.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace SpeedPuzzling\Web\Controller;
+
+use SpeedPuzzling\Web\Query\GetPuzzlesOverview;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class PuzzleServiceController extends AbstractController
+{
+    #[Route(path: '/chci-skladat-puzzle', name: 'puzzle_service', methods: ['GET'])]
+    public function __invoke(Request $request): Response
+    {
+        return $this->render('puzzle-service.html.twig');
+    }
+}

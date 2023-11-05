612120500a731599306ca942a146b67b68b77dc8
Jan Mikeš <j.mikes@me.com>
2023-11-05 12:30:29 +0100

    Set up Sentry

--- config/packages/sentry.php
+++ config/packages/sentry.php
@@ -1,7 +1,26 @@
 <?php declare(strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Config\SentryConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (SentryConfig $sentryConfig) {
+    $sentryConfig->dsn('%env(SENTRY_DSN)%');
 
+    $sentryConfig->tracing()
+        ->enabled(false);
+
+    $sentryConfig->registerErrorListener(false);
+
+    $sentryConfig->messenger()
+        ->enabled(true)
+        ->captureSoftFails(true);
+
+    $sentryConfig->options()
+        ->environment('%kernel.environment%')
+        ->sendDefaultPii(true);
+        /*
+        ->ignoreExceptions([
+            Symfony\Component\Security\Core\Exception\AccessDeniedException::class,
+            Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class,
+        ]);
+        */
 };

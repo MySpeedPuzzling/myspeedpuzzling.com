1e41929a7c90f8d9d70f691d019d025ffa5e52aa
Jan Mikeš <j.mikes@me.com>
2023-11-06 14:58:26 +0100

    Add solving time - search within puzzles (#14)
    * Draft: Add time with search capabilities
    
    * Design
    
    * Height tweak
    
    * Add time can be now filtered
    
    * fixup! Add time can be now filtered

--- src/Controller/AddTimeController.php
+++ src/Controller/AddTimeController.php
@@ -10,7 +10,10 @@ use SpeedPuzzling\Web\FormType\AddPuzzleSolvingTimeFormType;
 use SpeedPuzzling\Web\Message\AddPuzzleSolvingTime;
 use SpeedPuzzling\Web\Message\EditProfile;
 use SpeedPuzzling\Web\Query\GetPlayerProfile;
+use SpeedPuzzling\Web\Query\GetPuzzlesOverview;
+use SpeedPuzzling\Web\Results\PuzzleOverview;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -21,6 +24,7 @@ final class AddTimeController extends AbstractController
 {
     public function __construct(
         readonly private MessageBusInterface $messageBus,
+        readonly private GetPuzzlesOverview $getPuzzlesOverview,
     ) {
     }
 
@@ -35,19 +39,30 @@ final class AddTimeController extends AbstractController
             assert($data instanceof AddPuzzleSolvingTimeFormData);
             $userId = $user->getUserIdentifier();
 
-            $this->messageBus->dispatch(
-                AddPuzzleSolvingTime::fromFormData($userId, $data),
-            );
+            if ($data->puzzleId !== null) {
+                $this->messageBus->dispatch(
+                    AddPuzzleSolvingTime::fromFormData($userId, $data),
+                );
 
-            $this->addFlash(
-                'success',
-                'Skvělá práce! Skládání jsme zaznamenali.'
-            );
+                $this->addFlash(
+                    'success',
+                    'Skvělá práce! Skládání jsme zaznamenali.'
+                );
 
-            return $this->redirectToRoute('my_profile');
+                return $this->redirectToRoute('my_profile');
+            }
+
+            $addPuzzleSolvingTimeForm->addError(new FormError('Pro přidání času vyberte puzzle ze seznamu'));
+        }
+
+        /** @var array<string, array<PuzzleOverview>> $puzzlesPerManufacturer */
+        $puzzlesPerManufacturer = [];
+        foreach($this->getPuzzlesOverview->all() as $puzzle) {
+            $puzzlesPerManufacturer[$puzzle->manufacturerName][] = $puzzle;
         }
 
         return $this->render('add-time.html.twig', [
+            'puzzles' => $puzzlesPerManufacturer,
             'add_puzzle_solving_time_form' => $addPuzzleSolvingTimeForm,
         ]);
     }

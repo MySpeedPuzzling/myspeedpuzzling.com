efefa415199ef927abe026367397d55e3c277bf2
Jan Mikeš <j.mikes@me.com>
2023-11-10 23:14:12 +0100

    Stopwatches for user can be accessed via profile or stopwatch section (#18)

--- src/Controller/StopwatchStatusController.php
+++ src/Controller/StopwatchStatusController.php
@@ -3,35 +3,44 @@ declare(strict_types=1);
 
 namespace SpeedPuzzling\Web\Controller;
 
+use Auth0\Symfony\Models\User;
+use Psr\Log\LoggerInterface;
+use SpeedPuzzling\Web\Exceptions\PlayerNotFound;
+use SpeedPuzzling\Web\Query\GetPlayerProfile;
 use SpeedPuzzling\Web\Query\GetStopwatch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 final class StopwatchStatusController extends AbstractController
 {
     public function __construct(
-        private readonly GetStopwatch $getStopwatch
-    )
-    {
+        private readonly GetStopwatch $getStopwatch,
+        private readonly GetPlayerProfile $getPlayerProfile,
+        private readonly LoggerInterface $logger,
+    ) {
     }
 
     #[Route(path: '/stopky/{stopwatchId}', name: 'stopwatch_status', methods: ['GET'])]
-    public function __invoke(string $stopwatchId): Response
+    public function __invoke(string $stopwatchId, #[CurrentUser] User $user): Response
     {
-        $stopwatchDetail = $this->getStopwatch->byId($stopwatchId);
+        $userId = $user->getUserIdentifier();
 
-        $interval = $stopwatchDetail->totalSeconds;
+        try {
+            $player = $this->getPlayerProfile->byUserId($userId);
+        } catch (PlayerNotFound $e) {
+            $this->logger->critical('Stopwatch status - could not get player', [
+                'user_id' => $userId,
+                'exception' => $e,
+            ]);
 
-        if ($stopwatchDetail->lastEnd === null) {
-            $interval += (new \DateTimeImmutable())->getTimestamp() - $stopwatchDetail->lastStart->getTimestamp();
+            return $this->redirectToRoute('my_profile');
         }
 
         return $this->render('stopwatch.html.twig', [
-            'stopwatch' => $stopwatchDetail,
-            'hours_elapsed' => floor($interval / 3600),
-            'minutes_elapsed' => str_pad((string) floor(($interval / 60) % 60), 2, '0', STR_PAD_LEFT),
-            'seconds_elapsed' => str_pad((string) ($interval % 60), 2, '0', STR_PAD_LEFT),
+            'active_stopwatch' => $this->getStopwatch->byId($stopwatchId),
+            'stopwatches' => $this->getStopwatch->allForPlayer($player->playerId),
         ]);
     }
 }

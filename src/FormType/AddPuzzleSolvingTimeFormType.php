d5fb7c4d15b9a99d4b791c1454826bb3d7e9e06d
Jan Mikeš <j.mikes@me.com>
2023-12-17 21:07:07 +0100

    Fix: Number validation when adding new time for puzzles count

--- src/FormType/AddPuzzleSolvingTimeFormType.php
+++ src/FormType/AddPuzzleSolvingTimeFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -98,7 +99,7 @@ final class AddPuzzleSolvingTimeFormType extends AbstractType
             'help' => 'Prosím vyplňte pouze v případě, že jste výrobce nenašli v již existujícím seznamu',
         ]);
 
-        $builder->add('puzzlePiecesCount', TextType::class, [
+        $builder->add('puzzlePiecesCount', NumberType::class, [
             'label' => 'Počet dílků',
             'required' => false,
         ]);

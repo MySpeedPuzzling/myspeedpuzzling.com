d7244bf38e5a45579d70d4e07e04c5f62f11cb4d
Jan Mikeš <j.mikes@me.com>
2023-12-19 09:42:51 +0100

    Feature: Allow more time inputs (m:s, h:m:s)

--- tests/Value/SolvingTimeTest.php
+++ tests/Value/SolvingTimeTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SpeedPuzzling\Web\Tests\Value;
 
+use Generator;
 use PHPUnit\Framework\Attributes\DataProvider;
 use SpeedPuzzling\Web\Value\SolvingTime;
 use PHPUnit\Framework\TestCase;
@@ -18,10 +19,14 @@ class SolvingTimeTest extends TestCase
         self::assertSame($expected, $time->seconds);
     }
 
-    public static function provideFromInputData(): \Generator
+    /**
+     * @return Generator<array{string, int}>
+     */
+    public static function provideFromInputData(): Generator
     {
         yield ['1:01:06', 3666];
-
         yield ['01:06', 66];
+        yield ['1:1', 61];
+        yield ['1:1:1', 3661];
     }
 }

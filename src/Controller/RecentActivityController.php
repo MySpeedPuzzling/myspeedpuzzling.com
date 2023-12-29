108aaaa84831ba3b2459b86d19ee8d05087dd5e2
Jan Mikeš <j.mikes@me.com>
2023-12-29 18:03:58 +0100

    Feature: Added page with more recent activity (last 50)

---
+++ src/Controller/RecentActivityController.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace SpeedPuzzling\Web\Controller;
+
+use SpeedPuzzling\Web\Query\GetLastSolvedPuzzle;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class RecentActivityController extends AbstractController
+{
+    public function __construct(
+        readonly private GetLastSolvedPuzzle $getLastSolvedPuzzle,
+    ) {
+    }
+
+    #[Route(path: '/nedavna-aktivita', name: 'recent_activity', methods: ['GET'])]
+    public function __invoke(Request $request): Response
+    {
+        return $this->render('recent_activity.html.twig', [
+            'last_solved_puzzles' => $this->getLastSolvedPuzzle->limit(50),
+        ]);
+    }
+}

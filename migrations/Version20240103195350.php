a91487654f0b3969d34700fb5ddac40ec3d88adc
Jan Mikeš <j.mikes@me.com>
2024-01-03 20:54:53 +0100

    Tech: Changelog entity

---
+++ migrations/Version20240103195350.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpeedPuzzling\Web\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240103195350 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE changelog (id UUID NOT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, title TEXT NOT NULL, text TEXT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN changelog.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN changelog.date IS \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE changelog');
+    }
+}

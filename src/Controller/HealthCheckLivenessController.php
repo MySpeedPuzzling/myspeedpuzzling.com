1181f52a25cd05b8adb92a59171f9fcbb8fad0d1
Jan Mikeš <j.mikes@me.com>
2024-02-16 13:42:37 +0100

    Tech: Healthcheck controller

---
+++ src/Controller/HealthCheckLivenessController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace SpeedPuzzling\Web\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HealthCheckLivenessController extends AbstractController
+{
+    #[Route(path: '/-/health-check/liveness', methods: ['GET'])]
+    public function __invoke(): Response
+    {
+        return $this->json([
+            'status' => 'ok',
+            'time' => time(),
+        ]);
+    }
+}

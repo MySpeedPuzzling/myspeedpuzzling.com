7a190e35faf8b2577d9bb51902f1aacd9918e71e
Jan Mikeš <j.mikes@me.com>
2024-03-18 20:39:19 +0100

    Feature: Add my time to recent activity table

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 namespace SpeedPuzzling\Web\Controller;
 
 use SpeedPuzzling\Web\Query\GetLastSolvedPuzzle;
+use SpeedPuzzling\Web\Query\GetRanking;
 use SpeedPuzzling\Web\Query\GetStatistics;
+use SpeedPuzzling\Web\Services\RetrieveLoggedUserProfile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 final class HomepageController extends AbstractController
 {
     public function __construct(
         readonly private GetLastSolvedPuzzle $getLastSolvedPuzzle,
         readonly private GetStatistics $getStatistics,
+        readonly private RetrieveLoggedUserProfile $retrieveLoggedUserProfile,
+        readonly private GetRanking $getRanking,
     ) {
     }
 
@@ -27,15 +33,23 @@ final class HomepageController extends AbstractController
         name: 'homepage',
         methods: ['GET']
     )]
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, #[CurrentUser] UserInterface|null $user): Response
     {
         if ($request->getPathInfo() === '/') {
             return $this->redirectToRoute('homepage', ['_locale' => $request->getPreferredLanguage(['en', 'cs']) ?? 'en']);
         }
 
+        $playerProfile = $this->retrieveLoggedUserProfile->getProfile();
+        $userRanking = [];
+
+        if ($playerProfile !== null) {
+            $userRanking = $this->getRanking->allForPlayer($playerProfile->playerId);
+        }
+
         return $this->render('homepage.html.twig', [
             'last_solved_puzzles' => $this->getLastSolvedPuzzle->limit(5),
             'global_statistics' => $this->getStatistics->globally(),
+            'ranking' => $userRanking,
         ]);
     }
 }
--- src/Controller/RecentActivityController.php
+++ src/Controller/RecentActivityController.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 namespace SpeedPuzzling\Web\Controller;
 
 use SpeedPuzzling\Web\Query\GetLastSolvedPuzzle;
+use SpeedPuzzling\Web\Query\GetRanking;
+use SpeedPuzzling\Web\Services\RetrieveLoggedUserProfile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 final class RecentActivityController extends AbstractController
 {
     public function __construct(
         readonly private GetLastSolvedPuzzle $getLastSolvedPuzzle,
+        readonly private RetrieveLoggedUserProfile $retrieveLoggedUserProfile,
+        readonly private GetRanking $getRanking,
     ) {
     }
 
@@ -24,10 +30,18 @@ final class RecentActivityController extends AbstractController
         name: 'recent_activity',
         methods: ['GET'],
     )]
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, #[CurrentUser] UserInterface|null $user): Response
     {
+        $playerProfile = $this->retrieveLoggedUserProfile->getProfile();
+        $userRanking = [];
+
+        if ($playerProfile !== null) {
+            $userRanking = $this->getRanking->allForPlayer($playerProfile->playerId);
+        }
+
         return $this->render('recent_activity.html.twig', [
             'last_solved_puzzles' => $this->getLastSolvedPuzzle->limit(100),
+            'ranking' => $userRanking,
         ]);
     }
 }

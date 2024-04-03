98f524f5e38c516be07aeb47699e4494e9e61fca
Jan Mikeš <j.mikes@me.com>
2024-04-03 06:54:44 +0200

    Fix: Favorite players recent activity in group/pairs did not contain full results

--- src/Query/GetLastSolvedPuzzle.php
+++ src/Query/GetLastSolvedPuzzle.php
@@ -196,6 +196,19 @@ WITH favorite_player_ids AS (
     SELECT (json_array_elements_text(favorite_players)::UUID) AS player_id
     FROM player
     WHERE id = :playerId
+),
+filtered_puzzle_solving_time AS (
+    SELECT
+        puzzle_solving_time.id
+    FROM
+        puzzle_solving_time
+    LEFT JOIN LATERAL json_array_elements(puzzle_solving_time.team -> 'puzzlers') WITH ORDINALITY AS player_elem(player, ordinality) ON true
+    WHERE
+        puzzle_solving_time.player_id IN (SELECT player_id FROM favorite_player_ids)
+        OR (player_elem.player ->> 'player_id')::UUID IN (SELECT player_id FROM favorite_player_ids)
+    GROUP BY puzzle_solving_time.id, puzzle_solving_time.tracked_at
+    ORDER BY puzzle_solving_time.tracked_at DESC
+    LIMIT :limit
 )
 SELECT
     puzzle_solving_time.id as time_id,
@@ -207,36 +220,30 @@ SELECT
     puzzle_solving_time.player_id AS player_id,
     player.name AS player_name,
     player.country AS player_country,
-    pieces_count,
+    puzzle.pieces_count,
     puzzle_solving_time.comment,
     manufacturer.name AS manufacturer_name,
     puzzle.identification_number AS puzzle_identification_number,
     puzzle_solving_time.tracked_at AS tracked_at,
-    finished_at,
+    puzzle_solving_time.finished_at,
     puzzle_solving_time.finished_puzzle_photo AS finished_puzzle_photo,
     puzzle_solving_time.team ->> 'team_id' AS team_id,
-    CASE
-        WHEN puzzle_solving_time.team IS NOT NULL THEN JSON_AGG(
-            JSON_BUILD_OBJECT(
-                'player_id', player_elem.player ->> 'player_id',
-                'player_name', COALESCE(p.name, player_elem.player ->> 'player_name'),
-                'player_country', p.country
-            ) ORDER BY player_elem.ordinality
-        )
-    END AS players
-FROM puzzle_solving_time
+    JSON_AGG(
+        JSON_BUILD_OBJECT(
+            'player_id', player_elem.player ->> 'player_id',
+            'player_name', COALESCE(p_inner.name, player_elem.player ->> 'player_name'),
+            'player_country', p_inner.country
+        ) ORDER BY player_elem.ordinality
+    ) AS players
+FROM
+    puzzle_solving_time
 INNER JOIN puzzle ON puzzle.id = puzzle_solving_time.puzzle_id
 INNER JOIN player ON puzzle_solving_time.player_id = player.id
 INNER JOIN manufacturer ON manufacturer.id = puzzle.manufacturer_id
 LEFT JOIN LATERAL json_array_elements(puzzle_solving_time.team -> 'puzzlers') WITH ORDINALITY AS player_elem(player, ordinality) ON true
-LEFT JOIN player p ON p.id = (player_elem.player ->> 'player_id')::UUID
-WHERE
-    player.name IS NOT NULL AND
-    (puzzle_solving_time.player_id IN (SELECT player_id FROM favorite_player_ids) OR
-     (player_elem.player ->> 'player_id')::UUID IN (SELECT player_id FROM favorite_player_ids))
-GROUP BY puzzle_solving_time.id, puzzle.id, manufacturer.id, time, player.id, puzzle_solving_time.tracked_at
-ORDER BY puzzle_solving_time.tracked_at DESC
-LIMIT :limit
+LEFT JOIN player p_inner ON p_inner.id = (player_elem.player ->> 'player_id')::UUID
+INNER JOIN filtered_puzzle_solving_time ON puzzle_solving_time.id = filtered_puzzle_solving_time.id
+GROUP BY puzzle_solving_time.id, puzzle.id, player.id, manufacturer.id
 SQL;
 
         $data = $this->database

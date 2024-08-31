ba88c7b6a3f44421af018a75e715d9eaf2c62c00
Jan Mikeš <j.mikes@me.com>
2024-08-31 12:29:31 +0200

    API: add team + groups

--- src/Controller/Api/GetPlayerResultsController.php
+++ src/Controller/Api/GetPlayerResultsController.php
@@ -5,6 +5,10 @@ namespace SpeedPuzzling\Web\Controller\Api;
 
 use SpeedPuzzling\Web\Exceptions\PlayerNotFound;
 use SpeedPuzzling\Web\Query\GetPlayerSolvedPuzzles;
+use SpeedPuzzling\Web\Results\SolvedPuzzle;
+use SpeedPuzzling\Web\Services\PuzzlingTimeFormatter;
+use SpeedPuzzling\Web\Value\Puzzler;
+use SpeedPuzzling\Web\Value\SolvingTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,6 +18,7 @@ final class GetPlayerResultsController extends AbstractController
 {
     public function __construct(
         readonly private GetPlayerSolvedPuzzles $getPlayerSolvedPuzzles,
+        readonly private PuzzlingTimeFormatter $puzzlingTimeFormatter,
     ) {
     }
 
@@ -27,28 +32,75 @@ final class GetPlayerResultsController extends AbstractController
         }
 
         try {
-            $results = $this->getPlayerSolvedPuzzles->soloByPlayerId($playerId);
+            $soloResults = array_map(function(SolvedPuzzle $result): array {
+                return [
+                    'id' => $result->timeId,
+                    'time_seconds' => $result->time,
+                    'time' => $this->puzzlingTimeFormatter->formatTime($result->time),
+                    'ppm' => (new SolvingTime($result->time))->calculatePpm($result->piecesCount),
+                    'player_name' => $result->playerName,
+                    'puzzle_name' => $result->puzzleName,
+                    'puzzle_pieces' => $result->piecesCount,
+                    'puzzle_image' => $result->puzzleImage,
+                    'puzzle_brand' => $result->manufacturerName,
+                    'finished_at' => $result->finishedAt->format(DATE_ATOM),
+                    'solved_times' => $result->solvedTimes,
+                ];
+            }, $this->getPlayerSolvedPuzzles->soloByPlayerId($playerId));
+
+            $duoResults = array_map(function(SolvedPuzzle $result): array {
+                assert($result->players !== null);
+                $players = array_map(function(Puzzler $player): array {
+                    return [
+                        'id' => $player->playerId,
+                        'name' => $player->playerName,
+                    ];
+                }, $result->players);
+
+                return [
+                    'id' => $result->timeId,
+                    'time_seconds' => $result->time,
+                    'time' => $this->puzzlingTimeFormatter->formatTime($result->time),
+                    'ppm' => (new SolvingTime($result->time))->calculatePpm($result->piecesCount, 2),
+                    'players' => $players,
+                    'puzzle_pieces' => $result->piecesCount,
+                    'puzzle_image' => $result->puzzleImage,
+                    'puzzle_brand' => $result->manufacturerName,
+                    'finished_at' => $result->finishedAt->format(DATE_ATOM),
+                    'solved_times' => $result->solvedTimes,
+                ];
+            }, $this->getPlayerSolvedPuzzles->duoByPlayerId($playerId));
+
+            $teamResults = array_map(function(SolvedPuzzle $result): array {
+                assert($result->players !== null);
+                $players = array_map(function(Puzzler $player): array {
+                    return [
+                        'id' => $player->playerId,
+                        'name' => $player->playerName,
+                    ];
+                }, $result->players);
+
+                return [
+                    'id' => $result->timeId,
+                    'time_seconds' => $result->time,
+                    'time' => $this->puzzlingTimeFormatter->formatTime($result->time),
+                    'ppm' => (new SolvingTime($result->time))->calculatePpm($result->piecesCount, count($result->players)),
+                    'players' => $players,
+                    'puzzle_pieces' => $result->piecesCount,
+                    'puzzle_image' => $result->puzzleImage,
+                    'puzzle_brand' => $result->manufacturerName,
+                    'finished_at' => $result->finishedAt->format(DATE_ATOM),
+                    'solved_times' => $result->solvedTimes,
+                ];
+            }, $this->getPlayerSolvedPuzzles->teamByPlayerId($playerId));
         } catch (PlayerNotFound) {
             return $this->json(['error' => 'Player not found.'], Response::HTTP_NOT_FOUND);
         }
 
-        $data = [];
-
-        foreach ($results as $result) {
-            $data[] = [
-                'id' => $result->timeId,
-                'time_seconds' => $result->time,
-                'puzzle_name' => $result->puzzleName,
-                'puzzle_pieces' => $result->piecesCount,
-                'puzzle_image' => $result->puzzleImage,
-                'puzzle_brand' => $result->manufacturerName,
-                'type' => 'solo',
-                'team' => null,
-                'finished_at' => $result->finishedAt->format(DATE_ATOM),
-                'solved_times' => $result->solvedTimes,
-            ];
-        }
-
-        return $this->json($data);
+        return $this->json([
+            'solo' => $soloResults,
+            'duo' => $duoResults,
+            'team' => $teamResults,
+        ]);
     }
 }

edab53139278c2296574763cb238b0fa9211c9b7
Jan Mikeš <j.mikes@me.com>
2024-10-22 17:09:46 +0200

    Fix: Group times - missing names without player account

--- src/Query/GetTeamPlayers.php
+++ src/Query/GetTeamPlayers.php
@@ -58,7 +58,7 @@ SQL;
              * } $row
              */
 
-            if ($row['player_code'] === null) {
+            if ($row['player_name'] === null) {
                 continue;
             }
 

9d586cbf9b405c993f55ede8f80dcc412b78757c
Jan Mikeš <j.mikes@me.com>
2025-01-19 12:19:11 +0100

    Chart prototype: Use event instead of custom fork to change options Thanks to @smnandre See https://github.com/symfony/ux/issues/2508

--- assets/controllers/time_chart_controller.js
+++ assets/controllers/time_chart_controller.js
@@ -3,50 +3,60 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     connect() {
         this.element.addEventListener('chartjs:pre-connect', this._onPreConnect.bind(this));
+        this.element.addEventListener('chartjs:view-value-change', this._onViewValueChanged.bind(this));
     }
 
     disconnect() {
         this.element.removeEventListener('chartjs:pre-connect', this._onPreConnect.bind(this));
+        this.element.removeEventListener('chartjs:view-value-change', this._onViewValueChanged.bind(this));
     }
 
     _onPreConnect(event) {
         const config = event.detail.config;
 
-        config.options.maintainAspectRatio = false;
+        this.applyOptions(config.options);
+    }
 
-        if (!config.options.scales) {
-            config.options.scales = {};
+    _onViewValueChanged(event) {
+        const options = event.detail.options;
+
+        this.applyOptions(options);
+    }
+
+    applyOptions(options) {
+        options.maintainAspectRatio = false;
+
+        if (!options.scales) {
+            options.scales = {};
         }
-        if (!config.options.scales.y) {
-            config.options.scales.y = {};
+        if (!options.scales.y) {
+            options.scales.y = {};
         }
 
-        config.options.scales = {
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    stepSize: 900, // Step size of 15 minutes in seconds
-                    callback: function (value) {
-                        const hours = Math.floor(value / 3600);
-                        const minutes = Math.floor((value % 3600) / 60);
-                        const seconds = value % 60;
-                        return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds
-                            .toString()
-                            .padStart(2, '0')}`;
-                    },
-                }
+        options.scales.y = {
+            beginAtZero: true,
+            ticks: {
+                stepSize: 900, // Step size of 15 minutes in seconds
+                callback: function (value) {
+                    const hours = Math.floor(value / 3600);
+                    const minutes = Math.floor((value % 3600) / 60);
+                    const seconds = value % 60;
+                    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds
+                        .toString()
+                        .padStart(2, '0')}`;
+                },
             }
         };
 
-        if (!config.options.plugins) {
-            config.options.plugins = {};
+        if (!options.plugins) {
+            options.plugins = {};
         }
 
-        if (!config.options.plugins.tooltip) {
-            config.options.plugins.tooltip = {};
+        if (!options.plugins.tooltip) {
+            options.plugins.tooltip = {};
         }
 
-        config.options.plugins.tooltip.callbacks = {
+        options.plugins.tooltip.callbacks = {
             label: function (context) {
                 const value = context.raw;
                 const hours = Math.floor(value / 3600);

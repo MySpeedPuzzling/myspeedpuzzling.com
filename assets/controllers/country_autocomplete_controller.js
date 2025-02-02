f731b09eb1228cb88045bc8f627807cfb7ac9bfc
Jan Mikeš <j.mikes@me.com>
2025-02-02 22:27:56 +0100

    Prototype: New puzzle detail - filter by country

---
+++ assets/controllers/country_autocomplete_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    initialize() {
+        this._onPreConnect = this._onPreConnect.bind(this);
+    }
+
+    connect() {
+        this.element.addEventListener('autocomplete:pre-connect', this._onPreConnect);
+    }
+
+    disconnect() {
+        this.element.removeEventListener('autocomplete:pre-connect', this._onPreConnect);
+    }
+
+    _onPreConnect(event) {
+        event.detail.options.render = {
+            option: function(data, escape) {
+                return `<div>
+                            ${data.icon ? `<i class="${escape(data.icon)} me-1"></i> ` : ''}
+                            ${escape(data.text)}
+                        </div>`;
+            },
+            item: function(data, escape) {
+                return `<div>
+                            ${data.icon ? `<i class="${escape(data.icon)} me-1"></i> ` : ''}
+                            ${escape(data.text)}
+                        </div>`;
+            }
+        };
+    }
+}

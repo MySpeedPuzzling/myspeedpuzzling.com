303955d705a1e33b3dfb95f82f8f9bec56adb89d
Jan Mikeš <j.mikes@me.com>
2025-02-15 16:16:35 +0100

    Membership: Ladder countries filter - exclusive feature for members

--- src/Controller/LadderController.php
+++ src/Controller/LadderController.php
@@ -3,15 +3,19 @@ declare(strict_types=1);
 
 namespace SpeedPuzzling\Web\Controller;
 
+use Auth0\Symfony\Models\User;
 use SpeedPuzzling\Web\Query\GetFastestGroups;
 use SpeedPuzzling\Web\Query\GetFastestPairs;
 use SpeedPuzzling\Web\Query\GetFastestPlayers;
 use SpeedPuzzling\Web\Query\GetPlayersPerCountry;
+use SpeedPuzzling\Web\Services\RetrieveLoggedUserProfile;
 use SpeedPuzzling\Web\Value\CountryCode;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class LadderController extends AbstractController
 {
@@ -20,6 +24,8 @@ final class LadderController extends AbstractController
         readonly private GetFastestPairs $getFastestPairs,
         readonly private GetFastestGroups $getFastestGroups,
         readonly private GetPlayersPerCountry $getPlayersPerCountry,
+        readonly private RetrieveLoggedUserProfile $retrieveLoggedUserProfile,
+        readonly private TranslatorInterface $translator,
     ) {
     }
 
@@ -37,13 +43,21 @@ final class LadderController extends AbstractController
         ],
         name: 'ladder_country',
     )]
-    public function __invoke(Request $request, null|string $countryCode): Response
+    public function __invoke(Request $request, null|string $countryCode, #[CurrentUser] null|User $user): Response
     {
         if ($countryCode !== null) {
             $countryCode = CountryCode::fromCode($countryCode);
 
             if ($countryCode === null) {
                 throw $this->createNotFoundException();
+            }
+
+            $player = $this->retrieveLoggedUserProfile->getProfile();
+
+            if ($player === null || $player->activeMembership !== true) {
+                $this->addFlash('warning', $this->translator->trans('flashes.exclusive_membership_feature'));
+
+                return $this->redirectToRoute('ladder');
             }
         }
 

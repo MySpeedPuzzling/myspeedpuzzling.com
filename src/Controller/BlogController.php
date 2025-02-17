11da80b56941c9a2878599f96ce321e447c7fc76
Jan Mikeš <j.mikes@me.com>
2025-02-17 13:48:39 +0100

    Outage blog post ...

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace SpeedPuzzling\Web\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BlogController extends AbstractController
+{
+    public function __construct(
+    ) {
+    }
+
+    #[Route(
+        path: [
+            'cs' => '/blog/2025-02-17/nejvetsi-msp-vypadek',
+            'en' => '/en/blog/2025-02-17/the-biggest-msp-outage',
+        ],
+        name: 'blog',
+    )]
+    public function __invoke(): Response
+    {
+        return $this->render('blog.html.twig');
+    }
+}

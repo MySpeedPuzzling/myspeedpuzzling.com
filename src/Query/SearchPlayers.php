d6303d7657e7e6ebf7870cbafcb25de6e1a4172a
Jan Mikeš <j.mikes@me.com>
2025-04-06 23:43:06 +0200

    Players search improved

--- src/Query/SearchPlayers.php
+++ src/Query/SearchPlayers.php
@@ -25,17 +25,27 @@ SELECT
     name AS player_name,
     country AS player_country,
     code AS player_code,
-    CASE
+    (
+      CASE
         WHEN LOWER(code) = LOWER(:searchQuery) THEN 6 -- Exact match on code with diacritics
-        WHEN LOWER(name) = LOWER(:searchQuery) THEN 5 -- Exact match on name with diacritics
         WHEN LOWER(unaccent(code)) = LOWER(unaccent(:searchQuery)) THEN 4 -- Exact match on code without diacritics
+        WHEN LOWER(code) LIKE LOWER(:searchEndLikeQuery) OR LOWER(code) LIKE LOWER(:searchStartLikeQuery) THEN 4 -- Code starting/ending with search query (diacritics)
+        WHEN LOWER(unaccent(code)) LIKE LOWER(unaccent(:searchEndLikeQuery)) OR LOWER(unaccent(code)) LIKE LOWER(unaccent(:searchStartLikeQuery)) THEN 3 -- Code starting/ending with search query without diacritics
+        WHEN LOWER(code) LIKE LOWER(:searchFullLikeQuery) THEN 3 -- Partial match on code with search query (diacritics)
+        WHEN LOWER(unaccent(code)) LIKE LOWER(unaccent(:searchFullLikeQuery)) THEN 2 -- Partial match on code with search query without diacritics
+        ELSE 0
+      END
+      +
+      CASE
+        WHEN LOWER(name) = LOWER(:searchQuery) THEN 5 -- Exact match on name with diacritics
         WHEN LOWER(unaccent(name)) = LOWER(unaccent(:searchQuery)) THEN 3 -- Exact match on name without diacritics
-        WHEN LOWER(code) LIKE LOWER(:searchFullLikeQuery) THEN 3 -- Partial match on code with diacritics
-        WHEN LOWER(name) LIKE LOWER(:searchFullLikeQuery) THEN 2 -- Partial match on name with diacritics
-        WHEN LOWER(unaccent(code)) LIKE LOWER(unaccent(:searchFullLikeQuery)) THEN 2 -- Partial match on code without diacritics
-        WHEN LOWER(unaccent(name)) LIKE LOWER(unaccent(:searchFullLikeQuery)) THEN 1 -- Partial match on name without diacritics
-        ELSE 0 -- Other cases
-    END as match_score
+        WHEN LOWER(name) LIKE LOWER(:searchEndLikeQuery) OR LOWER(name) LIKE LOWER(:searchStartLikeQuery) THEN 3 -- Name starting/ending with search query (diacritics)
+        WHEN LOWER(unaccent(name)) LIKE LOWER(unaccent(:searchEndLikeQuery)) OR LOWER(unaccent(name)) LIKE LOWER(unaccent(:searchStartLikeQuery)) THEN 2 -- Name starting/ending with search query without diacritics
+        WHEN LOWER(name) LIKE LOWER(:searchFullLikeQuery) THEN 2 -- Partial match on name with search query (diacritics)
+        WHEN LOWER(unaccent(name)) LIKE LOWER(unaccent(:searchFullLikeQuery)) THEN 1 -- Partial match on name with search query without diacritics
+        ELSE 0
+      END
+    ) AS match_score
 FROM player
 WHERE LOWER(name) LIKE LOWER(:searchFullLikeQuery) OR LOWER(code) LIKE LOWER(:searchFullLikeQuery)
    OR LOWER(unaccent(name)) LIKE LOWER(unaccent(:searchFullLikeQuery)) OR LOWER(unaccent(code)) LIKE LOWER(unaccent(:searchFullLikeQuery))

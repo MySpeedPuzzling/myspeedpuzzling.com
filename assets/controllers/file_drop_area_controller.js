dc567b765adac8f28fe9733108f773331be9f722
Jan Mikeš <j.mikes@me.com>
2025-12-13 23:05:35 +0100

    Allow images editation before submitting form (#69)

--- assets/controllers/file_drop_area_controller.js
+++ assets/controllers/file_drop_area_controller.js
@@ -30,23 +30,49 @@ export default class extends Controller {
                             image.onload = function() {
                                 icon.className = 'file-drop-preview img-thumbnail rounded';
                                 icon.innerHTML = '<img src="' + image.src + '" alt="' + fileName + '">';
+
+                                // Add edit button if not exists
+                                let editBtn = fileArea[i].querySelector('.file-drop-edit-btn');
+                                if (!editBtn) {
+                                    // Get translation from image-editor controller if available
+                                    const imageEditorEl = fileArea[i].closest('[data-image-editor-edit-value]');
+                                    const editLabel = imageEditorEl ? imageEditorEl.dataset.imageEditorEditValue : 'Edit';
+
+                                    editBtn = document.createElement('button');
+                                    editBtn.type = 'button';
+                                    editBtn.className = 'file-drop-edit-btn btn btn-sm btn-outline-secondary';
+                                    editBtn.innerHTML = '<i class="bi bi-crop me-1"></i> ' + editLabel;
+                                    editBtn.setAttribute('data-action', 'click->image-editor#openEditor');
+
+                                    const chooseBtn = fileArea[i].querySelector('.file-drop-btn');
+                                    chooseBtn.parentNode.insertBefore(editBtn, chooseBtn.nextSibling);
+                                }
                             }
 
                         } else if (fileData.startsWith('data:video')) {
                             icon.innerHTML = '';
                             icon.className = '';
                             icon.className = 'file-drop-icon ci-video';
+                            removeEditButton(fileArea[i]);
 
                         } else {
                             icon.innerHTML = '';
                             icon.className = '';
                             icon.className = 'file-drop-icon ci-document';
+                            removeEditButton(fileArea[i]);
                         }
                     }
                     reader.readAsDataURL(input.files[0]);
                 }
 
             });
+        }
+
+        function removeEditButton(area) {
+            const editBtn = area.querySelector('.file-drop-edit-btn');
+            if (editBtn) {
+                editBtn.remove();
+            }
         }
     }
 }

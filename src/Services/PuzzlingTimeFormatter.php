ed8ab16b909047d92ca301d0a3ff71d9acfc1499
Jan Mikeš <j.mikes@me.com>
2026-01-01 15:18:58 +0100

    Timeformatter accepts null now

--- src/Services/PuzzlingTimeFormatter.php
+++ src/Services/PuzzlingTimeFormatter.php
@@ -18,16 +18,16 @@ readonly final class PuzzlingTimeFormatter
         );
     }
 
-    public function daysElapsed(int $interval): int
+    public function daysElapsed(null|int $interval): int
     {
-        $interval = (int) abs($interval);
+        $interval = (int) abs($interval ?? 0);
 
         return (int) floor($interval / (3600 * 24));
     }
 
-    public function hoursElapsed(int $interval, bool $overlap = true): string
+    public function hoursElapsed(null|int $interval, bool $overlap = true): string
     {
-        $interval = (int) abs($interval);
+        $interval = (int) abs($interval ?? 0);
         $hours = floor($interval / 3600);
 
         if ($overlap === false) {
@@ -37,15 +37,15 @@ readonly final class PuzzlingTimeFormatter
         return str_pad((string) $hours, 2, '0', STR_PAD_LEFT);
     }
 
-    public function minutesElapsed(int $interval): string
+    public function minutesElapsed(null|int $interval): string
     {
-        $interval = (int) abs($interval);
+        $interval = (int) abs($interval ?? 0);
         return str_pad((string) floor(((int) ($interval / 60)) % 60), 2, '0', STR_PAD_LEFT);
     }
 
-    public function secondsElapsed(int $interval): string
+    public function secondsElapsed(null|int $interval): string
     {
-        $interval = (int) abs($interval);
+        $interval = (int) abs($interval ?? 0);
         return str_pad((string) ($interval % 60), 2, '0', STR_PAD_LEFT);
     }
 }

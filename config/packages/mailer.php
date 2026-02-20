5073dd78152af839ec8bb7da9665d63a4429b060
Jan Mikeš <j.mikes@me.com>
2026-02-20 03:48:28 +0100

    Fix mailer from

--- config/packages/mailer.php
+++ config/packages/mailer.php
@@ -11,9 +11,6 @@ return App::config([
                 'transactional' => '%env(MAILER_TRANSACTIONAL_DSN)%',
                 'notifications' => '%env(MAILER_NOTIFICATIONS_DSN)%',
             ],
-            'envelope' => [
-                'sender' => 'robot@mail.myspeedpuzzling.com',
-            ],
             'headers' => [
                 'From' => 'MySpeedPuzzling <robot@mail.myspeedpuzzling.com>',
             ],
